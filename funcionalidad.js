26444eb77b818adc783e2b3d6db9974f777ebeed
Carlos <carlosprado101103@gmail.com>
2024-12-05 18:16:18 +0100

    Imagenes y funcionalidades

--- funcionalidad.js
+++ funcionalidad.js
@@ -14,3 +14,24 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+//Abrir el modal de informacion
+const infoImage = document.getElementById("info");
+const modal = document.getElementById("modal");
+const closeButton = document.getElementById("closeButton");
+
+// Abrir el modal
+infoImage.addEventListener("click", () => {
+  modal.style.display = "flex"; 
+});
+
+// Cerrar el modal
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none"; 
+});
+
+// Cerrar el modal al hacer clic fuera del contenido
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
