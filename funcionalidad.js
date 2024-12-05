be0d2f9db61c6a3239cf1d454cdf8306144d3004
Carlos <carlosprado101103@gmail.com>
2024-12-05 18:29:17 +0100

    Reloj

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,3 +1,21 @@
+//Reloj 
+function actualizarReloj() {
+    const ahora = new Date();
+    const horas = String(ahora.getHours()).padStart(2, '0');
+    const minutos = String(ahora.getMinutes()).padStart(2, '0');
+    const segundos = String(ahora.getSeconds()).padStart(2, '0');
+
+    const tiempo = `${horas}:${minutos}:${segundos}`;
+    document.getElementById('reloj').textContent = tiempo;
+}
+
+// Actualiza el reloj cada segundo
+setInterval(actualizarReloj, 1000);
+
+// Inicia el reloj
+actualizarReloj();
+
+//Menu de la pagina 
 document.addEventListener('DOMContentLoaded', function () {
     const menu = document.querySelector('.menu');
     const menuIcon = document.getElementById('menu-icon');
