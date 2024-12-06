1e421eeffe108d5b545f6282772e9327a21938ac
Carlos <carlosprado101103@gmail.com>
2024-12-06 10:54:15 +0100

    header terminado

--- funcionalidad.js
+++ funcionalidad.js
@@ -15,23 +15,22 @@ setInterval(actualizarReloj, 1000);
 // Inicia el reloj
 actualizarReloj();
 
-//Menu de la pagina 
-document.addEventListener('DOMContentLoaded', function () {
-    const menu = document.querySelector('.menu');
-    const menuIcon = document.getElementById('menu-icon');
+//Menu de la pagina
+const menu = document.getElementById('menu');
+const menuIcon = document.getElementById('menu-icon');
 
-    menuIcon.addEventListener('click', function () {
-        // Alternar la clase "open" para mostrar/ocultar el menú
-        menu.classList.toggle('open');
-
-        // Cambiar el icono del menú
-        if (menu.classList.contains('open')) {
-            menuIcon.src = 'assets/cerrar.png'; // Cambiar a "cerrar"
-        } else {
-            menuIcon.src = 'assets/menu.png'; // Cambiar a "menu"
-        }
-    });
+// Cambiar entre abrir y cerrar el menú al hacer clic
+menuIcon.addEventListener('click', () => {
+  menu.classList.toggle('open'); // Agregar/quitar la clase "open"
+  
+  // Cambiar el ícono dinámicamente
+  if (menu.classList.contains('open')) {
+    menuIcon.src = 'assets/cerrar.png'; // Ícono de cerrar
+  } else {
+    menuIcon.src = 'assets/menu.png'; // Ícono de abrir
+  }
 });
+
 //Abrir el modal de informacion
 const infoImage = document.getElementById("info");
 const modal = document.getElementById("modal");
